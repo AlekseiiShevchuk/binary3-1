8568cd51e55c4ac4224f5b0fc305faf34db15e9d
alekseiishevchuk <shevchuka@gmail.com>
2016-07-05 09:46:23 +0300

    реализован калькулятор без логгирования

--- index.php
+++ index.php
@@ -1,4 +1,107 @@
 <?php
 
+declare(strict_types = 1);
+
+
+use calculator\calculator;
 require __DIR__ . '/vendor/autoload.php';
 
+try {
+    $calculator = new calculator();
+    echo 'Суммируем "-50" и "130" = ';
+    echo $calculator->calculate(-50,130,'+');
+    echo '<br>';
+    echo 'Делаем ошибку в операторе:<br>';
+    echo $calculator->calculate(-50,130,'неверный оператор');
+    echo '<br>';
+
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+} catch (AssertionError $e) {
+    echo $e->getMessage(), "\n";
+} catch (DivisionByZeroError $e) {
+    echo $e->getMessage(), "\n";
+} catch (Throwable $e){
+    echo 'unforeseen throwable error \n';
+    echo $e->getMessage(), "\n";
+}
+
+try {
+    echo '<br><br>Вычитаем  "130" из "-50" = ';
+    echo $calculator->calculate(-50,130,'-');
+    echo '<br>';
+    echo 'Делаем ошибку в типе операнда:<br>';
+    echo $calculator->calculate(-50,130.25,'-');
+
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+} catch (AssertionError $e) {
+    echo $e->getMessage(), "\n";
+} catch (DivisionByZeroError $e) {
+    echo $e->getMessage(), "\n";
+} catch (Throwable $e){
+    echo 'unforeseen throwable error \n';
+    echo $e->getMessage(), "\n";
+}
+
+try {
+    echo '<br><br>Целочисленное деление "150" на "40" = ';
+    echo $calculator->calculate(150,40,'/');
+    echo '<br>';
+    echo 'Делим на ноль:<br>';
+    echo $calculator->calculate(150,0,'/');
+
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+} catch (AssertionError $e) {
+    echo $e->getMessage(), "\n";
+} catch (DivisionByZeroError $e) {
+    echo $e->getMessage(), "\n";
+} catch (Throwable $e){
+    echo 'unforeseen throwable error \n';
+    echo $e->getMessage(), "\n";
+}
+
+try {
+    echo '<br><br>Умножаем "150" на "40" = ';
+    echo $calculator->calculate(150,40,'*');
+    echo '<br>';
+    echo 'Неверный тип операндов при умножении:<br>';
+    echo $calculator->calculate(150.4,0.5,'*');
+
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+} catch (AssertionError $e) {
+    echo $e->getMessage(), "\n";
+} catch (DivisionByZeroError $e) {
+    echo $e->getMessage(), "\n";
+} catch (Throwable $e){
+    echo 'unforeseen throwable error \n';
+    echo $e->getMessage(), "\n";
+}
+
+try {
+    echo '<br><br>Возводим "2" в степень "20" = ';
+    echo $calculator->twoInPower(20);
+    echo '<br>';
+    echo 'Неверный тип операнда при возведении в степеь:<br>';
+    echo $calculator->twoInPower(20.5);
+
+} catch (TypeError $e) {
+    echo $e->getMessage(), "\n";
+} catch (AssertionError $e) {
+    echo $e->getMessage(), "\n";
+} catch (DivisionByZeroError $e) {
+    echo $e->getMessage(), "\n";
+} catch (Throwable $e){
+    echo 'unforeseen throwable error \n';
+    echo $e->getMessage(), "\n";
+}
+
+
+/* $calculator->setLogger(new class{
+     public function log(){
+         $LogString = date("Y-m-d H:i:s") . $this->operator;
+         file_put_contents('log.txt',$LogString);
+     }
+ });*/
--- src/calculator/calculator.php
+++ src/calculator/calculator.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php
 
 namespace calculator;
 
@@ -6,4 +6,67 @@ namespace calculator;
 class calculator
 {
 
+    private $a;
+    private $b;
+    private $operator;
+    private $logger;
+
+
+    public function calculate(int $a, int $b, string $operator): int
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->operator = $operator;
+
+        if
+        (
+            $this->operator !== '+' &&
+            $this->operator !== '-' &&
+            $this->operator !== '*' &&
+            $this->operator !== '/'
+        )
+        {
+            throw new \AssertionError('operator in calculator is incorrect, it must be "+" or "-" or "*" or "/" !');
+        }
+
+        if($this->operator === '+') {
+            return $this->plus();
+        }elseif($this->operator === '-'){
+            return $this->minus();
+        }elseif($this->operator === '*'){
+            return $this->multiplication();
+        }elseif($this->operator === '/'){
+            return $this->intDivision();
+        }
+    }
+
+    private function plus(): int
+    {
+        return $this->a + $this->b;
+    }
+
+    private function minus(): int
+    {
+        return $this->a - $this->b;
+    }
+
+    private function multiplication(): int
+    {
+        return $this->a * $this->b;
+    }
+
+    private function intDivision(): int
+    {
+        return intdiv($this->a,$this->b);
+    }
+
+    public function twoInPower(int $n): int
+    {
+        return 2**$n;
+    }
+
+    public function setLogger($logger)
+    {
+        $this->logger = $logger;
+    }
 }
